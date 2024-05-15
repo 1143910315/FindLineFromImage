ae77c0ef03aeee28070da794fd1456b14dca4b25
HapiFive <1143910315@qq.com>
2024-05-15 08:25:01 +0800

    读取图片内容并显示

---
+++ src/FindLineFromImage_zh_CN.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>主窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>unload picture</source>
+        <oldsource>unload my picture</oldsource>
+        <translation>图片加载失败</translation>
+    </message>
+</context>
+</TS>
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,6 +5,18 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    QImage image("image/input_image.jpg");
+    // 检查图片是否成功加载
+    if (!image.isNull()) {
+        // 将QImage转换为QPixmap，因为QLabel的setPixmap方法需要一个QPixmap对象
+        QPixmap pixmap = QPixmap::fromImage(image);
+        // 设置标签的图片
+        ui->label->setPixmap(pixmap);
+    }
+    else {
+        // 如果图片加载失败，可以在这里处理错误
+        ui->label->setText(tr("unload picture"));
+    }
 }
 
 MainWindow::~MainWindow() {
